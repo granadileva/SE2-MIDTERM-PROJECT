8d0f283788e008fda258660bb9269114873b8b69
Eva Granadil <evagranadil@gmail.com>
2025-11-28 20:21:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ connectDB();
 
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+app.options("*", cors());
 app.use(express.json());
 
 app.use(bodyParser.json());
